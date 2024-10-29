ec4e8d1b99184c80813d915f7225c417c96fabec
JBGruber <JohannesB.Gruber@gmail.com>
2024-10-29 09:03:35 +0100

    add checksum update to update script

--- update_v.sh
+++ update_v.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Update version
 api_url="https://api.github.com/repos/posit-dev/positron/tags"
-latest_tag=$(curl -s "$api_url" | grep -m 1 -oP '"name": "\K(.*?)(?=")')
+latest_tag=$(curl -s "$api_url" | grep -m 1 -oP '"name": "\K[0-9][^"]*(?=")')
 formatted_version=${latest_tag//-/_}
 sed -i "s/pkgver=.*/pkgver=$formatted_version/" PKGBUILD
+
+# Update checksum
+sha=$(curl -sL https://github.com/posit-dev/positron/releases/download/${latest_tag}/Positron-${latest_tag}.deb | sha256sum | awk '{print $1}')
+sed -i "s/sha256sums=.*/sha256sums=('$sha')/" PKGBUILD
 
 echo "Updated PKGBUILD with the latest version: $formatted_version"
